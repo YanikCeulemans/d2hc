0642520d057cc47ba9aa9d4c0199f11e65a159a4
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2014-10-25 00:17:33 +0200

    Updated hero card to read from json. Added test image element

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,15 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+    var ajax = document.querySelector('core-ajax'),
+        d2hc = document.querySelector('d2hc-hero-card');
+
+    ajax.addEventListener('core-response', function(e){
+        d2hc.setData(this.response[0]);
+    });
+
+    ajax.go();
+    
   });
 
 // wrap document so it plays nice with other libraries
